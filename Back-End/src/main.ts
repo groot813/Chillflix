295790cd12266a1be7db3acf879b9fe21062e8ae
Marc de Groot <marc.degroot@kpn.com>
2019-02-25 15:07:35 +0100

    test

---
+++ Back-End/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('CHILFLIX API')
+    .setDescription('Chillflix api for finding movies and managing them')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+
+const options = new DocumentBuilder()
+
+bootstrap();
